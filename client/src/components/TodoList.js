c29f6ebb15d14454332fa9e0cc80f19fc25fb349
Burke <beatty.burke@gmail.com>
2021-08-31 19:49:54 -0400

    todo list maps and has icons

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import TodoForm from './TodoForm'
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([])
@@ -12,13 +13,24 @@ function TodoList() {
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos)
-        console.log(...todos);
+       
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
     }
 
     return (
         <div>
             <h1>What's the plan for today?</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} />
         </div>
     )
 }
