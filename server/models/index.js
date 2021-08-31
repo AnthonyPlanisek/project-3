3be28ad5f7bae9acccdff179094ab79e72072d54
Luke Spang <lspang20@gmail.com>
2021-08-31 18:51:50 -0400

    seeder working

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,8 @@
 const User = require('./User');
+<<<<<<< HEAD
 const Task = require('./Task')
+=======
+const Task = require('./Task');
+
+>>>>>>> 18b3c59 (seeder working)
 module.exports = { User, Task };
