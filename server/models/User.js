18b3c5916cd14d1c5f8d043115a1c8d917da658b
Luke Spang <lspang20@gmail.com>
2021-08-31 18:51:50 -0400

    seeder working

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
     username: {
         type: String,
