2638822c7434ca11ff1194d3933d890531919651
Burke <beatty.burke@gmail.com>
2021-08-31 20:12:16 -0400

    added edit functionality

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -16,6 +16,21 @@ function TodoList() {
        
     };
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
+        )
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removeArr);
+    };
+
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if (todo.id === id) {
@@ -30,7 +45,7 @@ function TodoList() {
         <div>
             <h1>What's the plan for today?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 }
