477a3f549cf5947991e67fa7e210cb474db29c86
Luke Spang <lspang20@gmail.com>
2021-09-09 19:32:30 -0400

    graph and router

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,6 +3,7 @@ import plantrlogo from '../assets/images/plantrlogo.svg';
 import mySvg from '../assets/images/navpattern.svg';
 
 
+
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
@@ -12,7 +13,7 @@ export default function Navbar({ fixed }) {
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
               className="text-2xl font-bold leading-relaxed inline-block ml-0 mr-4 py-2 whitespace-nowrap uppercase text-secondary"
-              href="#pablo"
+              href="/"
             >
             <img className="img-responsive w-60  mt-2" src={plantrlogo} alt="logo"/>
             </a>
@@ -35,7 +36,7 @@ export default function Navbar({ fixed }) {
               <li className="nav-item">
                 <a
                   className="px-3 mt-2 py-2 flex items-center text-s  font-bold  font-poppins leading-snug text-black hover:opacity-75"
-                  href="#pablo"
+                  href="/"
                 >
                   <i className=" text-lg leading-lg text-dark opacity-75"></i><span className="ml-2">Home</span>
                 </a>
@@ -43,15 +44,15 @@ export default function Navbar({ fixed }) {
               <li className="nav-item">
                 <a
                   className="px-3 py-2 mt-2 flex items-center text-s  font-bold  font-poppins leading-snug text-black hover:opacity-75"
-                  href="#pablo"
+                  href="/productivity"
                 >
-                  <i className="fab fa-twitter text-lg leading-lg text-dark opacity-75"></i><span className="ml-2">Learn More</span>
+                  <i className="fab fa-twitter text-lg leading-lg text-dark opacity-75"></i><span className="ml-2">Start Planning</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
                   className="px-3 py-2 mt-2 flex items-center text-s  font-bold  font-poppins leading-snug text-black hover:opacity-75"
-                  href="#pablo"
+                  href="/signup"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-dark opacity-75"></i><span className="ml-2">Profile</span>
                 </a>
