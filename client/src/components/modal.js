d8d1d6f4b744758e2dad3989289ffc2578086b7d
Emmett Rushin <emmett.rushin@pclawtimematters.com>
2021-09-10 14:21:23 -0400

    description stays in modal

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 export default function Modal(props) {
-  console.log('PROPS ', props)
   const [todoDesc, setTodoDesc] = useState(props.todo.description || '')
 
   useEffect(() => {
