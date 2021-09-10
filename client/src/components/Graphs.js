d80fd019b50d69a507fd8f10325559d94aa93023
Luke Spang <lspang20@gmail.com>
2021-09-09 21:13:54 -0400

    merged with main, working site

--- client/src/components/Graphs.js
+++ client/src/components/Graphs.js
@@ -44,14 +44,7 @@ const Graphs = () => (
   <>
     <div className='header'>
       <h1 className='title'>Vertical Bar Chart</h1>
-      <div className='links'>
-        <a
-          className='btn btn-gh'
-          href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/VerticalBar.js'
-        >
-          Github Source
-        </a>
-      </div>
+      
     </div>
     <Bar data={data} options={options} />
   </>
